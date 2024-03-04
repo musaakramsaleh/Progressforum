b6d52ff2d10208997e385decab3d2c5530981e6c
musaakramsaleh <saleh01715972188@gmail.com>
2024-03-04 14:21:46 +0600

    done another push

--- js/formula.js
+++ js/formula.js
@@ -46,6 +46,7 @@ const post = async() =>{
         
     }); 
 }
+
 const latest = async ()=>{
    let  author = ''
    let date = ''
@@ -104,12 +105,56 @@ function hello(wr,lo){
    <span class="text-[#12132D99]"><i class="fa-regular fa-eye mr-2 "></i>${lo}</span>`
    special.appendChild(er)
 }
-
 search_key = async()=>{
+    // loading.classList.remove('hidden')
     const text = search.value
     const war = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${text}`)
     const we = await war.json()
-    console.log(we.posts)
+    const post = we.posts
+    
+    print.innerHTML = ''
+    post.forEach(record=>{
+      const title = record.title
+        const div = document.createElement('div')
+        div.classList.add('mt-4')
+        let stringWithoutSingleQuotes = record.title.replace(/'/g, '_');
+        div.innerHTML = `<div class="flex gap-6 items-start  bg-[#F3F3F5] p-10 rounded-3xl">
+        <div class="indicator">
+            <span class="indicator-item badge  bg-green-600" id = "my-color"></span> 
+            <div class="avatar">
+                <div class="w-[72px] h-[72px] rounded-2xl">
+                  <img src="${record.image}" />
+                </div>
+              </div>
+          </div>
+          <div>
+            <span class="font-medium text-[14px] text-[#12132DCC] mr-4">#${record.category}</span>
+            <span class="font-medium text-[14px] text-[#12132DCC] mr-4">#${record.author.name}</span>
+            <h2 class="text-[#12132D] font-bold text-xl">${record.title}</h2>
+            <p class="w-[596px] font-normal text-[#12132D99] mt-4">${record.description}</p>
+            <hr class="my-5 border-1 border-dashed border-[#12132D40]">
+            <div class="flex justify-between">
+                <div class="flex gap-7 text-[#12132D99] text-[16px] font-normal">
+                    <span><i class="fa-regular fa-comment-dots mr-4"></i>${record.comment_count}</span>
+                    <span><i class="fa-regular fa-eye mr-4"></i>${record.view_count}</span>
+                    <span><i class="fa-regular fa-clock mr-4"></i>${record.posted_time} min</span>
+                </div>
+                <div>
+                    <span class="text-white cursor-pointer"><i class="fa-solid fa-envelope-open w-7 h-7 rounded-full bg-green-400 flex justify-center items-center" onclick ="hello('${stringWithoutSingleQuotes}',${record.view_count})" ></i></span>
+                </div>
+            </div>
+          </div>
+        </div>`
+        loading.classList.remove('hidden')
+        setTimeout(()=>{
+          print.appendChild(div)
+        },2000)
+        setTimeout(()=>{
+          loading.classList.add('hidden')
+        },2000)
+        
+    })
+    
 }
 
 
@@ -119,4 +164,3 @@ setTimeout(()=>{
   
 },2000)
 latest()
-search_key()
